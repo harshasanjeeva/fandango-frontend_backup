8066b479043926b470092fa5b101418d84fdb9bc
harshasanjeeva <harshasanjiva@gmail.com>
2018-04-20 20:46:17 -0700

    harsha: template for fandango

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import {Router} from 'react-router-dom';
-import Root from './components/Root';
+// import Root from './components/Root';
 import './App.css';
 
 //import Button from 'react-bootstrap/lib/Button';
 import history from '../src/components/History';
 import {Route, Switch} from 'react-router-dom';
-import Profile from '../src/components/Profile';
+
 import {withRouter } from 'react-router-dom';
 
+import Home from '../src/components/Home';
 class App extends Component {
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from 'reactstrap';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import { persistStore } from 'redux-persist'
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import { PersistGate } from 'redux-persist/integration/react'
+//import persistor from './store/configureStore'
+import  store  from './store/configureStore'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//const store = configureStore();
+//persistStore(store);
+ReactDOM.render(
+    <Provider store={store.store}>
+    <PersistGate loading={null} persistor={store.persistor}>
+<App />
+</PersistGate>
+ </Provider>, document.getElementById('root'));
 registerServiceWorker();
