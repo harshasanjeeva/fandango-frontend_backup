a7e4f544675898eecac56b889fa86d606b0bf108
harshasanjeeva <harshasanjiva@gmail.com>
2018-04-24 20:07:15 -0700

    harsha: changed the css

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import {connect} from 'react-redux';
 import Login from "./Login";
 import history from './History';
 import Homemain from './Homemain'
+import Navbarheader from './Navbarmain'
 
 class Home extends Component {
 
@@ -30,46 +31,7 @@ class Home extends Component {
       <div >
   
       <div >
-        <Navbar color="dark" light expand="md">
-        <NavbarBrand href="/">
-
-        <img src={require('../images/1050x171.gif')} width="170" height="30" alt="" />
-
-       
-
-        </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/signup" to="/signup">Signup</NavLink>
-              </NavItem>
-              <NavItem>
-              <NavLink href="/login">Sign In</NavLink>
-            </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
+       <Navbarheader />
 
         <Homemain />
 
@@ -77,15 +39,7 @@ class Home extends Component {
 
       </div>
 
-          <Route exact path="/login" render={() => (
-              <div>
-                  <Login/>
-                  console.log("done with login");
 
-
-
-              </div>
-          )}/>
   
       </div>
     );
